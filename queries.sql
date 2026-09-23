e2efb3a17520f9751dd11ba5d62c1fc4dfd89862
goba <batuhan.goktas.dev@gmail.com>
2026-09-24 00:24:52 +0300

    s18d3 completed

---
+++ queries.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE IF NOT EXISTS doctor (
+    id BIGSERIAL PRIMARY KEY,
+    name CHARACTER VARYING,
+    surname CHARACTER VARYING,
+    proficiency CHARACTER VARYING
+);
+
+CREATE TABLE IF NOT EXISTS nurse (
+    id BIGSERIAL PRIMARY KEY,
+    name CHARACTER VARYING,
+    surname CHARACTER VARYING,
+    proficiency CHARACTER VARYING
+);
+
+CREATE TABLE IF NOT EXISTS patient (
+    id BIGSERIAL PRIMARY KEY,
+    name CHARACTER VARYING,
+    surname CHARACTER VARYING,
+    email CHARACTER VARYING,
+    complaint TEXT
+);
+
+CREATE TABLE IF NOT EXISTS surgery (
+    id BIGSERIAL PRIMARY KEY,
+    nurse_id BIGINT REFERENCES nurse(id),
+    patient_id BIGINT REFERENCES patient(id),
+    doctor_id BIGINT REFERENCES doctor(id)
+);
+
+CREATE TABLE IF NOT EXISTS operation (
+    id BIGSERIAL PRIMARY KEY,
+    patient_id BIGINT REFERENCES patient(id),
+    doctor_id BIGINT REFERENCES doctor(id)
+);
